676be40f9a736237d8329bb1fab77eea4eb4cca0
Maria <maria_sim_2509@mail.ru>
2021-03-24 22:55:50 +0300

    lesson_3_v.2.0

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('baskets/', include('basket.urls', namespace='baskets')),
     path('admin-staff/', include('adminapp.urls', namespace='admins')),
     path('', include('social_django.urls', namespace='social')),
+    path('order/', include('ordersapp.urls', namespace='order')),
 ]
 
 if settings.DEBUG:
